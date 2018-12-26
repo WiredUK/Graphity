ac5507addd44a5807571025727a6918f469d9e2c
David Glass <wireduk@gmail.com>
2018-12-26 12:13:34 +0000

    Tests for skip/take

--- test/Graphity.Tests/Middleware/BasicMiddlewareTests.cs
+++ test/Graphity.Tests/Middleware/BasicMiddlewareTests.cs
@@ -106,5 +106,49 @@ namespace Graphity.Tests.Middleware
             Assert.Equal("Sloth", item.Name);
             Assert.Null(item.LivesIn);
         }
+
+        [Fact]
+        public async Task Can_use_skip()
+        {
+            var client = _factory.CreateClient();
+
+            var query = new GraphQLQuery
+            {
+                Query = @"{animals(skip: 2) {id name}}"
+            };
+
+            var response = await client.PostAsJsonAsync("/api/graph", query);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsAsync<GraphExecutionResult<Animal>>();
+
+            Assert.Equal(2, result.Data["animals"].Count());
+
+            var item = result.Data["animals"].First();
+            Assert.Equal(3, item.Id);
+            Assert.Equal("Sloth", item.Name);
+            Assert.Null(item.LivesIn);
+        }
+
+        [Fact]
+        public async Task Can_use_take()
+        {
+            var client = _factory.CreateClient();
+
+            var query = new GraphQLQuery
+            {
+                Query = @"{animals(take: 2) {id name}}"
+            };
+
+            var response = await client.PostAsJsonAsync("/api/graph", query);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsAsync<GraphExecutionResult<Animal>>();
+
+            Assert.Equal(2, result.Data["animals"].Count());
+
+            var item = result.Data["animals"].First();
+            Assert.Equal(1, item.Id);
+            Assert.Equal("Dog", item.Name);
+            Assert.Null(item.LivesIn);
+        }
     }
 }
